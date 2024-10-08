080e5e95263c8223e9a72f984c94cf1af2954ba9
guilherme <guilhermeguerhardfantin@gmail.com>
2024-10-08 16:52:33 -0300

    implementei as mensagens de erro ou sucesso quando  o usuário tenta fazer login.

--- main.js
+++ main.js
@@ -4,17 +4,28 @@ let senha = document.querySelector("#senha");
 let form = document.querySelector("#login-form");
 let usuariocerto = "admin";
 let senhacerta = "123";
+let mensagem = document.querySelector("#mensagem");
 // Fim das variaveis
+
 function login(event) {
   event.preventDefault();
-
+  mensagem.className = "";
   if (nome.value == usuariocerto && senha.value == senhacerta) {
-    alert('Login realizado com sucesso!')
-    form.requestFullscreen()
-  }else{
-alert("Usuario ou senha incorretos")
-  }
-}
-// escutando o evento de evente de enviar o form
+    mensagem.textContent = "Login realizado com sucesso";
+    mensagem.classList.add("sucesso");
 
-form.addEventListener("submit", login)
+    form.reset();
+  } else {
+    mensagem.textContent = "Usuario ou senha incorretos";
+    mensagem.classList.add("erro");
+  }
+
+  // removendo mensagem de feedback apos x segundos
+
+  setTimeout(() => {
+    mensagem.className = "";
+  }, 2000);
+}
+
+// escutando o evento de evente de enviar o form
+form.addEventListener("submit", login);
